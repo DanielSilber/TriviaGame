e50ecf6e2bd1c8a52dc90a0ca9b552bcdedb2126
Daniel Silber <dsilber@Daniels-MBP.attlocal.net>
2018-02-26 22:44:29 -0800

    Final Push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
-// pos is position of where the user in the test or which question they're up to
+
 var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
-// this is a multidimensional array with 4 inner array elements with 5 elements inside them
-// if you don't want answers viewable in the code, then you should use PHP and mySQL database
+
 var questions = [
      ["Which city is in southern California?", "Los Angeles", "San Francisco", "Santa Cruz", "Eureka", "A"],
      ["Which album was recorded in the 90's?", "The Dark Side of the Moon", "The Rise and Fall of Ziggy Stardust", "Amnesiac", "Remain in Light", "C"],
@@ -9,7 +8,7 @@ var questions = [
      ["Which US city is credited as the cultural origin of Techno?", "Denver", "Detroit", "Austin", "Seattle", "B"],
      ["Which brand does not make cameras?", "Nikon", "tin man can", "Canon", "Leica", "B"]
      ];
-// this get function is short for the getElementById function  
+ 
 function get(x){
   return document.getElementById(x);
 }
@@ -18,10 +17,10 @@ function renderQuestion(){
   if(pos >= questions.length){
     test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
     get("test_status").innerHTML = "Test completed";
-    // resets the variable to allow users to restart the test
+   
     pos = 0;
     correct = 0;
-    // stops rest of renderQuestion function running when test is completed
+    
     return false;
   }
   get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
@@ -38,22 +37,53 @@ function renderQuestion(){
     test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
 function checkAnswer(){
-  // use getElementsByName because we have an array which it will loop through
+  
   choices = document.getElementsByName("choices");
   for(var i = 0; i < choices.length; i++){
     if(choices[i].checked){
       choice = choices[i].value;
     }
   }
-  // checks if answer matches the correct choice
+  
   if(choice == questions[pos][5]){
-    //each time there is a correct answer this value increases
+   
     correct++;
   }
-  // changes position of which character user is on
+  
   pos++;
-  // then the renderQuestion function runs again to go to next question
+  
   renderQuestion();
 }
 window.addEventListener("load", renderQuestion, false);
+
+// timer -- trouble getting it to start counting down from 10 seconds
+var countDown
+var timeLeft = 10;
+var elem = document.getElementById('#timer');
+
+var timerId = setInterval(countdown, 1000);
+
+function countDown() {
+    
+  if (timeLeft == 0) {
+    clearTimeout(timerId);
+    doSomething();
+  } else {
+    document.getElementById('#timer');
+    
+  }
+}
+function SubmitFunction(){
+    $('#submitted').html('Time is up!');
+      document.getElementById('mcQuestion').submit();
+     }
+ 
+      countDown(10,$('#display'));
+
+      //var interVal=  setInterval(function () {
+          //seconds = parseInt(timer % 60, 10);
+          //seconds = seconds < 10 ? "0" + seconds : seconds;
+          //$(display).html("<b>" " + seconds + "s" + "</b>");
+ 
+
 
