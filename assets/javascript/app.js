f74cb1eb0068bdc8c2f2e3ba6a05c6698be111e1
Daniel Silber <dsilber@Daniels-MBP.attlocal.net>
2018-02-26 17:00:00 -0800

    Push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,124 +1,61 @@
-var quizQuestions;
-var questionNumber;
-var buildQuiz;
-var showResults;
-var buildQuiz;
-var userAnswer;
-
-(function () {
-    function buildQuiz() {
-        var output = [];
-        quizQuestions.forEach((currentQuestion, questionNUmber) => {
-
-            var answers = [];
-            for (letter in currentQuestion.answers) {
-
-                answers.push(
-                    `<label>
-                <input type="radio" name="questions${questionNumber}"
-
-        value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-            
-            </label>`
-                );
-            }
-
-            output.push(
-                `<div class="question">
-        ${currentQuestion.question} </div>
-        <div class="answers">
-        ${answers.join("")} </div>`
-            );
-        });
-
-        quizContainer.innerHtml = output.join("");
-    }
-
-
-function showResults() {
-
-    var answerContainers = quizContainer.querySelectorAll(".answers");
-
-    let numCorrect = 0;
-
-    quizQuestions.forEach((currentQuestion, questionNumber) => {
-        var answerContainer = answerContainers[questionNumber];
-        var selector = `input[name=question${questionNumber}]:checked`;
-        var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        
-        if (userAnswer === currentQuestion.correctAnswer) {
-            numCorrect++;
-
-            answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        else {
-            answerContainers[questionNumber].style.color = 'red';
-        }
-    });
-
-    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+// if you don't want answers viewable in the code, then you should use PHP and mySQL database
+var questions = [
+     ["Which city is in southern California?", "Los Angeles", "San Francisco", "Santa Cruz", "Eureka", "A"],
+     ["Which album was recorded in the 90's?", "The Dark Side of the Moon", "The Rise and Fall of Ziggy Stardust", "Amnesiac", "Remain in Light", "C"],
+     ["Where do plums grow?", "moon rocks", "sea weed", "mud", "trees", "D"],
+     ["Which US city is credited as the cultural origin of Techno?", "Denver", "Detroit", "Austin", "Seattle", "D"],
+     ["Which brand does not make cameras?", "Nikon", "tin man can", "Canon", "Leica", "B"]
+     ];
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
 }
-
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
-var submitButton = document.getElementById("submit");
-var quizQuestions = [
-    {
-     question1: "Which city is in southern California?",
-        answers: {
-            a: "Los Angeles",
-            b: "San Francisco",
-            c: "Santa Cruz",
-            d: "Eureka",
-        },
-        correctAnswer: "a"
-    },
-    {
-        question2: "Which album was recorded in the 90's?",
-        answers: {
-            a: "The Dark Side of the Moon",
-            b: "The Rise and Fall of Ziggy Stardust",
-            c: "Amnesiac",
-            d: "Remain in Light",
-        },
-        correctAnswer: "c"
-    },
-    {
-        question3: "Where do plums grow?",
-        answers: {
-            a: "moon rocks",
-            b: "sea weed",
-            c: "mud",
-            d: "trees",
-        },
-        correctAnswer: "d"
-    },
-    {
-        question4: "Which US city is credited as the cultural origin of Techno?",
-        answers: {
-            a: "Denver",
-            b: "Detroit",
-            c: "Austin",
-            d: "Seattle",
-        },
-        correctAnswer: "d"
-    },
-    {
-        question5: "Which brand does not make cameras?",
-        answers: {
-            a: "Nikon",
-            b: "tin man can",
-            c: "Canon",
-            d: "Leica",
-        },
-        correctAnswer: "b"
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+    get("test_status").innerHTML = "Test completed";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.legnth;
+    question = questions[pos][0];
+    chA = questions[pos][1];
+    chC = questions[pos][3];
+    chD = questions[pos][4];
+    chD = questions[pos][4];
+    chB = questions[pos][2];
+    test.innerHTML = "<h3>"+question+"</h3>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='D'> "+chD+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='D'> "+chD+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i = 0; i < choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
     }
-];
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos][5]){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+window.addEventListener("load", renderQuestion, false);
 
-buildQuiz();
-
-submitButton.addEventListener("click", showResults);
-
-})();
